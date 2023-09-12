85c91e9131d73ea056205b7efc2d98612ad6e8a2
casteealc <emilorenti@gmail.com>
2023-09-12 09:16:33 -0600

    prueba

--- prueba/Login.js
+++ prueba/Login.js
@@ -10,17 +10,23 @@ form.addEventListener("submit", e=>{
     let entrar = false
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     parrafo.innerHTML = ""
-    if(nombre.Value.length <6){
-        warnings += 'El nombre no es valido <br>'
+    if(nombre.value.length < 6){
+        warnings += 'El nombre no es válido <br>'
         entrar = true
     }
     if(!regexEmail.test(email.value)){
-        warnings += 'El email no es valido <br>'
+        warnings += 'El email no es válido <br>'
         entrar = true
     }
 
-    if(pass.value.length <8){
-        warnings += 'La contraseña no es valido <br>'
+    if(pass.value.length < 8){
+        warnings += 'La contraseña no es válida <br>'
         entrar = true
+    }
+s
+    if (entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
     }
 })
